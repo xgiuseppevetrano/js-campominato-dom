af67725ecf2fa811a6da2ea32f71d554dd685ee4
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-09 17:54:45 +0200

    add while to create userNumber array

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// 2. Chiedo con un prompt all'utente (100 - 16) volte di inserire una numero alla volta compreso tra 1 e 100
-//  2a. Controllo che l'utente inserisca solo numeri e che questi numeri siano effettivamente compresi tra 1 e 100 (do while)
-//  2b. Controllo che il numero inserito NON sia stato già detto
-
-// 3. SE il numero inserito dall'utente è presente nell'array di numeri generati
-//    ALLORA la partita termina
-//    SE il numero inserito dall'utente NON è presente nell'array di numeri generati
-//    ALLORA si continua chiedendo all'utente un nuovo numero
-
-// 4. La partita termina SE l'utente inserisce un numero presente nell'array di numeri generati
-//    o SE inserisci tutti i numeri che non sono presenti nell'array di numeri generati
-
-// 5. Al termine della partita il software deve comunicare il punteggio (alert)
-//  5a. Il punteggio sarà dato da il numero di volte che l'utente ha inserito un numero consentito
-
 // BONUS
 // 1. Chiedo all'utente il livello di difficoltà del gioco
 //  1a. SE difficoltà 0 => numeri casuali tra 1 e 100
@@ -42,3 +27,29 @@ while (bombs.length < 16) {
 }
 
 console.log(bombs);
+
+// 2. Chiedo con un prompt all'utente (100 - 16) volte di inserire una numero alla volta compreso tra 1 e 100
+const userNumber = [];
+let controller = false;
+
+while (userNumber.length < (100 - 16) || controller === false) {
+    const askUserNumber = Number(prompt("Inserisci un numero tra 1 e 100"));
+    // 2a. SE il numero inserito dall'utente è presente nell'array di numeri generati
+    //    ALLORA la partita termina
+    if (bombs.includes(askUserNumber)) {
+        controller = true;
+        console.log("Hai perso!");
+        //    SE il numero inserito dall'utente NON è presente nell'array di numeri generati e
+        //    SE l'utente ha inserito solo numeri e quest'ultimi sono compresi tra 1 e 100 e non sono stati già detti
+        //    ALLORA si continua chiedendo all'utente un nuovo numero
+    } else if (!isNaN(askUserNumber) && askUserNumber >= 1 && askUserNumber <= 100 && !userNumber.includes(askUserNumber)) {
+        userNumber.push(askUserNumber);
+    }
+}
+
+// 4. La partita termina SE l'utente inserisce un numero presente nell'array di numeri generati
+//    o SE inserisci tutti i numeri che non sono presenti nell'array di numeri generati
+
+// 5. Al termine della partita il software deve comunicare il punteggio (alert)
+//  5a. Il punteggio sarà dato da il numero di volte che l'utente ha inserito un numero consentito
+
