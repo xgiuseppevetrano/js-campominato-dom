a9e079e85fca5e0cb7cd5b8c2e00eda744dca54b
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-09 21:51:55 +0200

    add bonus

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// BONUS
-// 1. Chiedo all'utente il livello di difficoltà del gioco
-//  1a. SE difficoltà 0 => numeri casuali tra 1 e 100
-//  1a. SE difficoltà 1 => numeri casuali tra 1 e 80
-//  1a. SE difficoltà 2 => numeri casuali tra 1 e 50
-
 /*---------------
     FUNCTION
 ---------------*/
@@ -17,28 +11,41 @@ function genericNumber(min, max) {
 -----------*/
 // 1. Genero un array di 16 numeri casuali tra 1 e 100
 const bombs = [];
+let difficultyLevel;
+let possibleNumbers;
+//  1a. Chiedo all'utente il livello di difficoltà tra 0 e 2
+do {
+    difficultyLevel = Number(prompt("Choose a difficulty level between 0, 1 and 2"))
+} while (isNaN(userNumber) || userNumber < 0 || userNumber > 2);
+//  1b. SE il livello di difficoltà è uguale a 0 - 1 - 2
+//      ALLORA i numeri possibili saranno 100 - 80 - 50
+if (difficultyLevel === 0) {
+    possibleNumbers = 100;
+} else if (difficultyLevel === 1) {
+    possibleNumbers = 80;
+} else if (difficultyLevel === 2) {
+    possibleNumbers = 50;
+}
 
 while (bombs.length < 16) {
-    let numbers = genericNumber(1, 100);
-    //  1a. I numeri generati NON possono essere duplicati
+    let numbers = genericNumber(1, possibleNumbers);
+    //  1c. I numeri generati NON possono essere duplicati
     if (!bombs.includes(numbers)) {
         bombs.push(numbers);
     }
 }
 
-console.log(bombs);
-
 // 2. Chiedo con un prompt all'utente (100 - 16) volte di inserire una numero alla volta compreso tra 1 e 100
 const userNumber = [];
-let controller = false;
+let controller = true;
 
-while (userNumber.length < 84 || controller === false) {
-    const askUserNumber = Number(prompt("Inserisci un numero tra 1 e 100"));
+while (userNumber.length < (possibleNumbers - 16) && controller) {
+    const askUserNumber = Number(prompt("Enter a number between 1 and 100"));
     // 2a. SE il numero inserito dall'utente è presente nell'array di numeri generati
     //    ALLORA la partita termina e il punteggio sarà dato da il numero di volte che l'utente ha inserito un numero consentito
     if (bombs.includes(askUserNumber)) {
-        controller = true;
-        alert("Hai perso! Il tuo punteggio è:", userNumber.length);
+        controller = false;
+        alert(`You lost, your score is: ${userNumber.length}`);
         // 2b. SE il numero inserito dall'utente NON è presente nell'array di numeri generati e
         //     SE l'utente ha inserito solo numeri e quest'ultimi sono compresi tra 1 e 100 e non sono stati già detti
         //     ALLORA si continua chiedendo all'utente un nuovo numero
@@ -50,6 +57,6 @@ while (userNumber.length < 84 || controller === false) {
 
 // 3. SE l'utente inserisce tutti i numeri che non sono presenti nell'array di numeri generati
 //    ALLORA vince e il punteggio sarà dato da il numero di volte che l'utente ha inserito un numero consentito
-if (userNumber.lenght === 84) {
-    alert("Hai vinto! Il tuo punteggio è:", userNumber.length);
+if (userNumber.lenght === (100 - 16)) {
+    alert(`You won, your score is: ${userNumber.length}`);
 }
