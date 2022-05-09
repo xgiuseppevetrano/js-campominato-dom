81250edd0667a9f7e387ede1f3238dab74a4f3de
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-09 16:14:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// 1. Creo una funzione per generare 16 numeri casuali tra 1 e 100
+//  1a. I numeri generati NON possono essere duplicati
+//  1b. Salvo questi numeri in un array
+
+// 2. Chiedo con un prompt all'utente (100 - 16) volte di inserire una numero alla volta compreso tra 1 e 100
+//  2a. Controllo che l'utente inserisca solo numeri e che questi numeri siano effettivamente compresi tra 1 e 100 (do while)
+//  2b. Controllo che il numero inserito NON sia stato già detto
+
+// 3. SE il numero inserito dall'utente è presente nell'array di numeri generati
+//    ALLORA la partita termina
+//    SE il numero inserito dall'utente NON è presente nell'array di numeri generati
+//    ALLORA si continua chiedendo all'utente un nuovo numero
+
+// 4. La partita termina SE l'utente inserisce un numero presente nell'array di numeri generati
+//    o SE inserisci tutti i numeri che non sono presenti nell'array di numeri generati
+
+// 5. Al termine della partita il software deve comunicare il punteggio (alert)
+//  5a. Il punteggio sarà dato da il numero di volte che l'utente ha inserito un numero consentito
+
+// BONUS
+// 1. Chiedo all'utente il livello di difficoltà del gioco
+//  1a. SE difficoltà 0 => numeri casuali tra 1 e 100
+//  1a. SE difficoltà 1 => numeri casuali tra 1 e 80
+//  1a. SE difficoltà 2 => numeri casuali tra 1 e 50
